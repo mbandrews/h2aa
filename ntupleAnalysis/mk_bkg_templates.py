9cf9c5fb59139ed4b69d72cf3e1b3fe8232fbe43
Michael <michael.andrews@cern.ch>
2021-09-29 10:26:17 -0500

    Calculate fhgg from th+MC instead of data

--- ntupleAnalysis/mk_bkg_templates.py
+++ ntupleAnalysis/mk_bkg_templates.py
@@ -52,21 +52,41 @@ sub_campaign = 'bdtgtm0p96_relChgIsolt0p07_etalt1p44/nom-%s'%sel # bdt > -0.96, 
 #sub_campaign = 'bdtgtm0p96_relChgIsolt0p09_etalt1p44/nom-%s'%sel # bdt > -0.96, relChgIso < 0.09
 #sub_campaign = 'bdtgtm0p96_relChgIsolt0p08_etalt1p44/nom-%s'%sel # bdt > -0.96, relChgIso < 0.08
 #campaign_noptwgts = 'bkgNoPtWgts-Era04Dec2020v2/%s'%sub_campaign
-campaign_noptwgts = 'bkgNoPtWgts-Era22Jun2021v1/%s'%sub_campaign
+#campaign_noptwgts = 'bkgNoPtWgts-Era22Jun2021v1/%s'%sub_campaign
+#campaign_noptwgts = 'bkgNoPtWgts-Era22Jun2021v2/%s'%sub_campaign # v1 but with bin 50MeV [not used]
+#campaign_noptwgts = 'bkgNoPtWgts-Era22Jun2021v3/%s'%sub_campaign # duplicate of v1 but with SFs on hgg template
+campaign_noptwgts = 'bkgNoPtWgts-Era22Jun2021v4/%s'%sub_campaign # duplicate of v3, but with fhgg derived from SM br(hgg)
 #campaign_ptwgts = 'bkgPtWgts-Era04Dec2020v2/%s'%sub_campaign
-campaign_ptwgts = 'bkgPtWgts-Era22Jun2021v1/%s'%sub_campaign
+#campaign_ptwgts = 'bkgPtWgts-Era22Jun2021v1/%s'%sub_campaign
+#campaign_ptwgts = 'bkgPtWgts-Era22Jun2021v2/%s'%sub_campaign # v1 but with bin 50MeV [not used]
+#campaign_ptwgts = 'bkgPtWgts-Era22Jun2021v3/%s'%sub_campaign # duplicate of v1 but with SFs on hgg template
+campaign_ptwgts = 'bkgPtWgts-Era22Jun2021v4/%s'%sub_campaign # duplicate of v3, but with fhgg from SM br(hgg)
 if doRun2: campaign_ptwgts += '/%s'%run2dir
 
+# f_hgg = ( intlumi * xs * br * N_hgg,sel / N_hgg,gen ) / N_sel,data
+# where the above equality is assumed to hold identically for sel = evt sel + (mH-SR & ma-SB) and sel = evt sel + (mH-SR & ma-SR)
+# so that we can derive it from (mH-SR & ma-SB) and use it for (mH-SR & ma-SR)
+hgg_campaign = 'sg-Era22Jun2021v4/%s'%sub_campaign # sg-Era22Jun2021v2 + interp masses (v3) + ss with SFs
+skim_campaign = 'ggNtuples-Era20May2021v1_ggSkim-v2' # for getting mc nevtsgen
+# https://twiki.cern.ch/twiki/bin/view/LHCPhysics/CERNYellowReportPageAt1314TeV2014
+# total SM inclusive higgs prodn: 50.94, gluglu-only: 43.92
+# Calculated using: https://docs.google.com/spreadsheets/d/1D8ztbh1WtCnSGJ_E0KQC0I5CEN0g61HQU4-XcfSm3iQ/edit#gid=368154701
+xs_hgg = 51.04 # pb, SM total inclusive
+xs_hgg_hi = 55.55 # pb, SM total inclusive + th uncerts
+xs_hgg_lo = 39.56 # pb, gluglu only
+nevtsgen = {}
+
 br_hgg = 2.27e-3 # BR(h->gg)
-nhgg = {
-        '2016': 214099989.445038,
-        '2017': 214099989.445038,
-        '2018': 214099989.445038
-        } # N wgt gen evts
+#nhgg = {
+#        '2016': 214099989.445038,
+#        '2017': 214099989.445038,
+#        '2018': 214099989.445038
+#        } # N wgt gen evts
 # Official lumis: https://twiki.cern.ch/twiki/bin/view/CMS/TWikiLUM
 #intlumi = {'2016': 36.33e3, #35.92e3,
 #           '2017': 41.53e3,
 #           '2018': 59.74e3} # /pb.
+
 # Estimated lumis modulo missing lumis: https://docs.google.com/spreadsheets/d/1wmDcb88uJfgJakIE9BfKfHXU_sb1ldy6NArZZzr0fRk/edit#gid=0
 # Calculated using:
 #   [1] cmslpc:~mba2012/nobackup/h2aa/CMSSW_10_5_0/src/h2aa/crab/run_getLumis.py
@@ -75,46 +95,16 @@ intlumi = {'2016': 36.25e3,
            '2017': 41.53e3,
            '2018': 58.75e3} # /pb. Run2: 136.53/pb
 
-#flo_nom = 0.645897591246
-#flo_ins = [0.5040, flo_nom, 0.7910]
+'''
+[DEPRECATED]
 # Input f_hggs. Calculated using get_fhgg.py
-'''
-fhggs = {
-        '2016nom': [0.                       , 1.29409e-03, 1.29409e-03 + 2.71978e-0],
-        '2016inv': [8.39948e-03 - 3.85741e-03, ]
-
-        '2017nom': [1.24404e-02 - 5.74877e-03, 1.24404e-02, 1.24404e-02 + 5.74877e-03],
-        '2017inv': [],
-
-        '2018nom': [0.                       , 1.23262e-02, 1.23262e-02 + 4.81454e-02],
-        '2018inv': [5.98173e-03 - 3.48104e-03, 5.98173e-03, 5.98173e-03 + 3.48104e-03]
-         }
-# Make sure f_hgg scenarios are in increasing order and positive-valued
-for k in fhggs:
-    assert (fhggs[k][1] > fhggs[k][0]) and (fhggs[k][1] < fhggs[k][2])
-    if fhggs[k][0] < 0:
-        fhggs[k][0] = 0.
-    print(fhggs[k])
-fhggs = {
-        '2016nom': [1.29409e-03, 2.71978e-03],
-        '2016inv': [8.39948e-03, 3.85741e-03],
-
-        '2017nom': [1.24404e-02, 5.74877e-03],
-        '2017inv': [5.26839e-03, 2.54058e-03],
-
-        '2018nom': [1.23262e-02, 4.81454e-02],
-        '2018inv': [5.98173e-03, 3.48104e-03]
-         } # [nom fit value, fit uncert]
-fhggs = {
-        'nom': [3.28431e-03, 2.77196e-03],
-        'inv': [1.92478e-03, 2.46728e-03]
-         } # [nom fit value, fit uncert]
-'''
 fhggs = {
         'nom': [2.88903e-03, 2.78887e-03],
         'inv': [1.42361e-03, 1.22762e-03],
          } # [nom fit value, fit uncert]
+'''
 
+#runs = ['2018']
 runs = ['2016', '2017', '2018']
 
 for r in runs:
@@ -159,7 +149,15 @@ for r in runs:
         # raw mh-SR
         # dont use full run2 folder since getting mh-SR (unwgtd) counts per yr
         workdir = '%s/%s/Templates'%(input_dir, campaign_noptwgts)
-        samples = [sample_data, sample_hgg]
+        #samples = [sample_data, sample_hgg]
+        samples = [sample_data]
+        mh_regions = ['sr']
+        load_hists(h, hf, samples, mh_regions, distns, ma_blind_input, workdir)
+
+        ## raw mh-SR, hgg
+        # Use hgg template with phoid+trg SFs applied
+        workdir = '%s/%s/Templates/systNom_nom'%(input_dir, hgg_campaign)
+        samples = [sample_hgg]
         mh_regions = ['sr']
         load_hists(h, hf, samples, mh_regions, distns, ma_blind_input, workdir)
 
@@ -195,36 +193,31 @@ for r in runs:
         h[ksb2sr].Scale(sb2sr_norm)
         print('.. h[%s].Integral(): %f'%(ksb2sr, h[ksb2sr].Integral()))
 
+        '''
+        [DEPRECATED]:
         # Calculate fhgg over full, unblinded (2d-pt) evt yields (same as for flo)
         # If only using blinded 2d-ma, can give skewed estimates of true hgg xsec yield
         khgg_2dpt = '%s_sr_%s'%(sample_hgg, k2dpt)
         ksr_2dpt = '%s_sr_%s'%(sample_data, k2dpt)
-
-        '''
-        # Loop over hgg xsecs: nom: total SM inclusive: 50.94, syst: gluglu-only: 43.92
-        # If doing flo_nom, only need to do nom xsec
-        #xs_nom, xs_syst = 50.94, 43.92 # [pb] gluglu only:48.58->43.92
-        #xs_syst, xs_nom = 50.94, 43.92 # [pb] gluglu only:48.58->43.92
-        #xsecs = [xs_nom, xs_syst] if flo_in == flo_nom else [xs_nom]
-        #for xsec_hgg in xsecs:
         '''
 
-        # Loop over f_hggs: nominal fit from get_fhgg +/- fit uncert
-        # If varying f_sblo, only use nominal f_hgg, else loop over all f_hgg syst shifts
-        #fhgg_nom = fhggs[r+sel][0] # nominal is 1st element
-        #fhgg_err = fhggs[r+sel][1] # fit uncert is 2nd element
-        fhgg_nom = fhggs[sel][0] # nominal is 1st element
-        fhgg_err = fhggs[sel][1] # fit uncert is 2nd element
-        if flo_in == flo_nom:
-            fhggs_ = [fhgg_nom-fhgg_err if fhgg_nom > fhgg_err else 0., fhgg_nom, fhgg_nom+fhgg_err]
-        else:
-            fhggs_ = [fhgg_nom]
-        for fhgg in fhggs_:
+        # Get total hgg gen events for this sample/year
+        nevtsgen[sample_hgg] = get_mcgenevents(sample_hgg, input_dir, hgg_campaign, skim_campaign)
+        print('.. nevtsgen[%s]: %f'%(sample_hgg, nevtsgen[sample_hgg]))
+        # Get hgg 2dma yield in diag_lo_hi-blinded region (mH-SR & ma-SB)
+        khgg = '%s_sr_%s'%(sample_hgg, k2dma)
+        khgg_blind = '%s-%s'%(khgg, ma_blind_norm)
+        hblind[khgg_blind] = h[khgg].Clone()
+        hblind[khgg_blind].SetName(khgg_blind)
+        blind_hist(hblind[khgg_blind], to_blind=ma_blind_norm)
 
-            assert fhgg >= 0.
-            print('  >> Doing f_hgg:',fhgg)
+        # Loop over hgg xsecs:
+        # If doing non-flo_nom, only need to do nom xsec
+        xsecs = [xs_hgg_lo, xs_hgg, xs_hgg_hi] if flo_in == flo_nom else [xs_hgg]
+        for xs in xsecs:
 
             '''
+            [DEPRECATED]:
             fhgg = br_hgg*xsec_hgg*intlumi[r]*nevts[khgg_2dpt]/nhgg[r]
             fhgg /= nevts[ksr_2dpt]
             print('  .. fhgg, 2017:', fhgg)
@@ -233,28 +226,38 @@ for r in runs:
             else:
                 fhgg = 3.84666817207e-05 if xsec_hgg == xs_nom else 4.46150447826e-05
             '''
+            print('  >> Doing xs_hgg:',xs)
+
+            # Calculate fhgg using diag_lo_hi-blinded region:
+            # f_hgg = ( intlumi * xs * br * N_hgg,sel / N_hgg,gen ) / N_sel,data
+            # where N_sel,data = sr_blind_yield, for sel = (mH-SR & ma-SB)
+            #print('nhgg_gen:',nevtsgen[sample_hgg])
+            #print('hblind[ksr_blind].Integral():',sr_blind_yield)
+            #print('nhggsel:',hblind[khgg_blind].Integral())
+            #print('intlumi[r]:',intlumi[r])
+            #print('xs_hgg:',xs)
+            #print('br_hgg:',br_hgg)
+            fhgg = ( intlumi[r] * xs * br_hgg * hblind[khgg_blind].Integral() ) / ( nevtsgen[sample_hgg] * sr_blind_yield )
+
+            # fsb is then the complement
             fsb = 1.-fhgg
             print('  .. fhgg:',fhgg)
             print('  .. fsb:',fsb)
 
-            #'''
-            # Normalize hgg template s.t. diag_lo_hi-blinded hgg yield matches corresponding yield in data(mh-SR)
+            # Now, normalize full 2dma hgg template
+            # Normalize hgg template s.t. diag_lo_hi-blinded hgg yield matches corresponding yield in data(mh-SR & ma-SB)
             # Required to preserve norm of total SB+hgg template in diag_lo_hi-blinded 2d-ma
-            khgg = '%s_sr_%s'%(sample_hgg, k2dma)
-            khgg_blind = '%s-%s'%(khgg, ma_blind_norm)
-            hblind[khgg_blind] = h[khgg].Clone()
-            hblind[khgg_blind].SetName(khgg_blind)
-            blind_hist(hblind[khgg_blind], to_blind=ma_blind_norm)
-            hgg_norm = sr_blind_yield/hblind[khgg_blind].Integral()
-            h[khgg].Scale(hgg_norm)
+            hgg2sr_norm = sr_blind_yield/hblind[khgg_blind].Integral()
+            #h[khgg].Scale(hgg2sr_norm) # will get overwritten!! apply as a scale when adding hgg to total bkg instead, as below
 
-            # Combine existing mh-SBlo+hi template with hgg template:
+            # Combine existing full 2dma mh-SBlo+hi template with hgg template:
             # fsb*data(mh-SBlo+hi) + fgg*hgg(mh-SR)
             ksb2sr_hgg = '%s_sb2sr+hgg_%s'%(sample_data, k2dma)
             h[ksb2sr_hgg] = h[ksb2sr].Clone()
             h[ksb2sr_hgg].SetName(ksb2sr_hgg)
             h[ksb2sr_hgg].Scale(fsb)
-            h[ksb2sr_hgg].Add(h[khgg], fhgg)
+            #h[ksb2sr_hgg].Add(h[khgg], fhgg)
+            h[ksb2sr_hgg].Add(h[khgg], fhgg*hgg2sr_norm)
             print('  .. h[%s].Integral(): %f'%(ksb2sr_hgg, h[ksb2sr_hgg].Integral()))
 
             # Draw
@@ -299,9 +302,13 @@ for r in runs:
             elif flo_in > flo_nom:
                 syst_str += '_floUp'
             # hgg yield
-            if fhgg < fhgg_nom:
+            #if fhgg < fhgg_nom:
+            #    syst_str += '_hggDn'
+            #elif fhgg > fhgg_nom:
+            #    syst_str += '_hggUp'
+            if xs < xs_hgg:
                 syst_str += '_hggDn'
-            elif fhgg > fhgg_nom:
+            elif xs > xs_hgg:
                 syst_str += '_hggUp'
 
             #print(hblind.keys())
